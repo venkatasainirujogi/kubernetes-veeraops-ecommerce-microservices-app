63a2edd201eb5b84f7d1eeb84dbccc328cb6713b
venkatasainirujogi <venkatasainirujogi@gmail.com>
2026-08-31 11:24:17 +0530

    Update backend.tf

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "devncloudtechdevops"
+    bucket       = "cahy-sepovsss"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
